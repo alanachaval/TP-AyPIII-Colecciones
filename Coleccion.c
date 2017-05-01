599c9c4e4f5ec5fd647cddb38b48900ff0a48497
alanachaval <achavalalann@gmail.com>
2017-05-01 11:11:52 -0300

    Inicial

---
+++ Coleccion.c
@@ -0,0 +1,4 @@
+#include "Coleccion.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
---
+++ Coleccion.h
@@ -0,0 +1,30 @@
+#ifndef __Coleccion_H__
+#define __Coleccion_H__
+
+/*
+ *
+ */
+
+typedef struct _Coleccion {
+
+} Coleccion;
+
+/*
+ *	Funcionalidades Requeridas
+ *
+ *	Agregar elemento a la collección (collection_add).
+ *	Quitar elemento de la collección  (collection_remove).
+ *	Inicializar la colección con un conjunto de valores (collection_init).
+ *	Iterar la colección (collection_iterate).
+ *	Encontrar un valor dentro de la colección (void* collection_find( Collection* c, void* value,...)).
+ *	Seleccionar un conjunto de elementos de la colección (collection_select( Collection* c, Collection* dst, ...)).
+ *	Colectar elementos de una colección (void collection_collect( Collection* c, Collection* dst, ...)).
+ *	Filtar elementos de una colección (void collection_filter( Collection* c, ...)).
+ *	Reducir a derecha una colección (definir).
+ *	Reducir a izquierda una colección (definir).
+ *	Unir dos colecciones.
+ *	Intersección de colecciones.
+ */
+
+#endif //__Coleccion_H__
+
